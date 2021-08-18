f8d464bb6815a8e1eed342262e2086b9342d2611
Миша <ishuckn5@gmail.com>
2021-08-19 00:47:28 +0200

    db

--- PycharmProjects/Pvp_bot/loader.py
+++ PycharmProjects/Pvp_bot/loader.py
@@ -1,9 +1,29 @@
-
+import psycopg2
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-
 from data import config
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+from psycopg2 import OperationalError
+
+def create_connection(db_name, db_user, db_password, db_host, db_port):
+    connection = None
+    try:
+        connection = psycopg2.connect(
+            database=db_name,
+            user=db_user,
+            password=db_password,
+            host=db_host,
+            port=db_port,
+        )
+        print("Connection to PostgreSQL DB successful")
+    except OperationalError as e:
+        print(f"The error '{e}' occurred")
+    return connection
+
+
+connection = create_connection(
+    "Pvp_bot_db", "postgres", "postgres123", "127.0.0.1", "5432"
+)
