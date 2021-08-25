c3023b617b237c3c7faa778454f43dc0da7a684e
OneZeroZeroOneOne <intheendreallife@gmail.com>
2021-08-25 16:23:46 +0300

    fix

--- PycharmProjects/Pvp_bot/handlers/users/blackjack/blackjack_lobby.py
+++ PycharmProjects/Pvp_bot/handlers/users/blackjack/blackjack_lobby.py
@@ -96,16 +96,17 @@ async def bot_blackjack_revenge(call:CallbackQuery, state: FSMContext):
     await repo.set_player_state(data.get('game_id'), data.get('user_id'), 'REVENGE')
 
     states = await repo.get_players_states(data.get('game_id'))
-    if states[0][0] == 'REVENGE' and states[1][0] == 'REVENGE' and states != None:
+    if states == None:
+        await call.message.edit_text(
+            f"Игрок отказался от реванша. Для доступа к меню используйте команду /start.\n",
+            parse_mode=types.ParseMode.HTML, )
+    elif states[0][0] == 'REVENGE' and states[1][0] == 'REVENGE' and states != None:
         await call.message.edit_text(
             f"Вы приняли предложение реванша.\n",
             parse_mode=types.ParseMode.HTML)
         await repo.create_revenge_game(data.get('game_id'))
         await start_blackjack(data.get('game_id'))
-    elif states == None:
-        await call.message.edit_text(
-            f"Игрок отказался от реванша. Для доступа к меню используйте команду /start.\n",
-            parse_mode=types.ParseMode.HTML, )
+    
     else:
         await call.message.edit_text(
             f"Вы отправили предложение реванша. Ожидаем решения игрока.\n",
