585562f9fd683ac4d5c2482751155da85e1f7f90
OneZeroZeroOneOne <intheendreallife@gmail.com>
2021-08-27 16:47:45 +0300

    fix

--- PycharmProjects/Pvp_bot/data/config.py
+++ PycharmProjects/Pvp_bot/data/config.py
@@ -1,4 +1,4 @@
 #BOT_TOKEN = "1820522016:AAEv5XcZ-etJbQKRZ0MtaEuJVr8wcyZg2Mk"  # Забираем значение типа str
-BOT_TOKEN = "1820522016:AAGRTSbyFooivgPplZGDdUmmizmN5GqcJjQ"
+BOT_TOKEN = "1791180696:AAHCn1ngJabqexTBAnC79vKUpHSGbFu98cI"
 ADMINS = [462567885]  # Тут у нас будет список из админов
 IP = "localhost"  # Тоже str, но для айпи адреса хоста
--- PycharmProjects/Pvp_bot/handlers/tiktaktoe/tiktaktoe.py
+++ PycharmProjects/Pvp_bot/handlers/tiktaktoe/tiktaktoe.py
@@ -1,3 +1,4 @@
+from utils.db_api.user.user_repo import UserRepo
 from utils.db_api.common.balance_repo import BalanceRepo
 from handlers.tiktaktoe.services.check_winner import check_winner
 from utils.db_api.create_asyncpg_connection import create_conn
@@ -22,33 +23,45 @@ async def cancel_search_tiktaktoe(call:CallbackQuery):
 
 
 @dp.callback_query_handler(tiktaktoe_callback.filter(), state="*")
-async def start_tiktaktoe(call:CallbackQuery, callback_data: dict, ):
+async def start_tiktaktoe_random(call:CallbackQuery, callback_data: dict, ):
     rates_id = int(callback_data['rates_id'])
     conn = await create_conn("conn_str")
     repo = TikTakToeRepo(conn)
     players = await repo.get_lobby_players(rates_id, call.from_user.id)
     if players:
-        await call.message.delete()
+        await repo.delete_users_lobby(players[0]['id'])
+        user_repo = UserRepo(conn)
+        user = await user_repo.get_user(call.from_user.id)
+        players = [players[0], user]
+        #await call.message.delete()
         await call.message.answer("Ваша игра найдена")
-        game_id = await repo.create_game(rates_id, players[0]['user_id'])
-        await repo.delete_users_lobby(players[0]['user_id'])
-        await repo.add_user_to_game(players[0]['user_id'], game_id, "X")
-        await repo.add_user_to_game(call.from_user.id, game_id, "O")
-        await repo.create_cells(9 , game_id)
-        charapters = {} 
-        charapters[call.from_user.id] = "O"
-        charapters[players[0]['user_id']] = "X"
-        cells = await repo.get_game_cells(game_id)
-        await repo.add_user_steps(9, game_id, user_ids=[players[0]['user_id'], call.from_user.id])
-        cells = [Cell(cell['id'], "🪑" if not cell['user_id'] else charapters[cell['user_id']], cell['user_id'], cell['game_id'])  for cell in cells]
-        message = await call.message.answer("Вы играете за \"O\"", reply_markup=draw(cells))
-        await repo.set_game_user_message_id(call.from_user.id, game_id, message.message_id)
-        message = await call.bot.send_message(chat_id=players[0]['user_id'], text="Вы играете за \"X\"", reply_markup=draw(cells))
-        await repo.set_game_user_message_id(players[0]['user_id'], game_id, message.message_id)
+        await create_tiktaktoe(repo, players, rates_id, call.message)
     else:
         await repo.add_lobby_user(call.from_user.id, rates_id)
-        await call.message.edit_text("Поиск игры начат, для отмены нажмите \"Отменить\" ", reply_markup=cancel_search_tiktaktoe_keyb())
+        await call.message.answer("Поиск игры начат, для отмены нажмите \"Отменить\" ", reply_markup=cancel_search_tiktaktoe_keyb())
     await conn.close()
+
+
+
+async def create_tiktaktoe(repo: TikTakToeRepo, players: list, rates_id: int, message):
+    game_id = await repo.create_game(rates_id, players[0]['id'])
+    round_id = await repo.create_game_round(game_id, players[0]['id'])
+    await repo.create_cells(9 , round_id)
+    charapters = {}
+    charapters_variants = ["X", "O"]
+    for i in range(0, len(players)):
+        charapters[players[i]['id']] = charapters_variants[i]
+        await repo.add_user_to_game(players[i]['id'], game_id, charapters_variants[i])
+        await repo.add_user_to_round(players[i]['id'], round_id, charapters_variants[i])
+    cells = await repo.get_game_cells(round_id)
+    await repo.add_user_steps(9, round_id, user_ids=[i['id'] for i in players])
+    cells = [Cell(cell['id'], "🪑" if not cell['user_id'] else charapters[cell['user_id']], cell['user_id'], cell['round_id'])  for cell in cells]
+    for i in players:
+        message = await message.bot.send_message(chat_id=i['id'], text=f"Вы играете за {charapters[i['id']]}", reply_markup=draw(cells))
+        await repo.set_game_round_user_message_id(i['id'], round_id, message.message_id)
+
+
+
 
 
 
@@ -56,51 +69,90 @@ async def start_tiktaktoe(call:CallbackQuery, callback_data: dict, ):
 async def make_step_tiktaktoe(call:CallbackQuery, callback_data: dict):
     conn = await create_conn("conn_str")
     repo = TikTakToeRepo(conn)
-    cell = await repo.get_game_cell_by_id(int(callback_data['cell_id']))
-    game = await repo.get_game_by_id(int(callback_data["game_id"]))
+    cell = await repo.get_round_cell_by_id(int(callback_data['cell_id']))
+    round = await repo.get_round_by_id(int(callback_data["round_id"]))
+    game = await repo.get_game_by_id(round["game_id"])
     if not game:
         await conn.close()
-        return await call.message.answer("game not found")
+        return await call.message.answer("game round not found")
     if not cell:
         await conn.close()
         return await call.message.answer("cell not found")
     if not game['is_end']:
-        if game['user_step_id'] != call.from_user.id:
+        if round['user_step_id'] != call.from_user.id:
             return await call.answer("Сейчас не ваш ход", show_alert=True)
         if not cell['is_busy']:
-            game_users = await repo.get_game_users(game['id'])
+            game_users = await repo.get_round_users(round['id'])
             await repo.take_cell(call.from_user.id, cell['id'])
-            cells: dict = await repo.get_game_cells(game['id'])
-            cells = [Cell(cell['id'], next((i for i in game_users if i['user_id'] == cell['user_id']), None)['character'] if cell['user_id'] != None else "🪑" , cell['user_id'], cell['game_id'])  for cell in cells]
+            cells: list = await repo.get_game_cells(round['id'])
+            cells = [Cell(cell['id'], next((i for i in game_users if i['id'] == cell['user_id']), None)['character'] if cell['user_id'] != None else "🪑" , cell['user_id'], cell['round_id'])  for cell in cells]
             if_end, winner_id = check_winner(cells)
             b_repo = BalanceRepo(conn)
             rates = await b_repo.get_rates_by_id(game['rates_id'])
             if if_end:
-                await repo.set_game_end(game['id'])
-                for game_user in game_users:
-                    if game_user['user_id'] == winner_id:
-                        await call.bot.send_message(text=f"Вы победили, вам было начислено на счет {rates['value']} фишек", chat_id=game_user['user_id'])
-                    else:
-                        await call.bot.send_message(text=f"Вы проиграли, у вас было снято со счета {rates['value']} фишек", chat_id=game_user['user_id'])
-            next_step = await repo.get_step(game['id'], game['step']+1)
-            if next_step:
-                await repo.set_game_user_step(next_step['sequence'], next_step['user_id'], game['id'])
-            else:
-                await repo.set_game_end(game['id'])
-                if not if_end:
+                await repo.set_round_end(round['id'])
+                await repo.set_round_winner_id(winner_id, round['id'])
+                r_w = await repo.get_round_winners(winner_id, game['id'])
+                if len(r_w) == 2:
+                    text=""
+                    await repo.set_game_end(game['id'])
+                    r_w = await repo.get_round_winners(winner_id, game['id'])
                     for game_user in game_users:
-                        await call.bot.send_message(text=f"Игра окончена ничьей, баланс всех учасников не изменился", chat_id=game_user['user_id'])  
-            for game_user in game_users:
-                if next_step:
-                    text = "Ход соперника.\nВы играете за {}".format(game_user['character'])
-                    if game_user['user_id'] == next_step['user_id']:
-                        text = "Ваш Ход.\nВы играете за {}".format(game_user['character'])
+                        await call.bot.delete_message(chat_id=game_user['id'], message_id=game_user['message_id'])
+                        if game_user['id'] == winner_id:
+                            await call.bot.send_message(text=f"Вы победили.\nВам было начислено на счет {rates['value']} фишек", chat_id=game_user['id'])
+                        else:
+                            await call.bot.send_message(text=f"Вы проиграли.\nУ вас было снято со счета {rates['value']} фишек", chat_id=game_user['id'])
                 else:
-                    text = "Игра окончена"
-                await call.bot.edit_message_text(text=text,
-                                                message_id=game_user['message_id'],
-                                                chat_id=game_user['user_id'],
-                                                reply_markup=draw(cells))
+                    round_id = await repo.create_game_round(game['id'], game_users[0]['id'], sequence=round['sequence'] + 1)
+                    await repo.create_cells(9 , round_id)
+                    charapters = {}
+                    charapters_variants = ["X", "O"]
+                    for i in range(0, len(game_users)):
+                        charapters[game_users[i]['id']] = charapters_variants[i]
+                        await repo.add_user_to_round(game_users[i]['id'], round_id, charapters_variants[i])
+                    cells = await repo.get_game_cells(round_id)
+                    await repo.add_user_steps(9, round_id, user_ids=[i['id'] for i in game_users])
+                    cells = [Cell(cell['id'], "🪑" if not cell['user_id'] else charapters[cell['user_id']], cell['user_id'], cell['round_id'])  for cell in cells]
+                    for i in game_users:
+                        message = await call.message.bot.edit_message_text(chat_id=i['id'],message_id=i['message_id'], text=f"Раунд {round['sequence'] + 1}\n.Вы играете за {charapters[i['id']]}", reply_markup=draw(cells))
+                        await repo.set_game_round_user_message_id(i['id'], round_id, message.message_id)
+            else:
+                next_step = await repo.get_step(round['id'], round['step']+1)
+                if next_step:
+                    await repo.set_game_round_user_step(next_step['sequence'], next_step['user_id'], round['id'])
+                    for game_user in game_users:
+                        text = "Раунд: {}.\nХод соперника.\nВы играете за {}".format(round['sequence'],game_user['character'])
+                        if game_user['id'] == next_step['user_id']:
+                            text = "Раунд: {}.\nВаш Ход.\nВы играете за {}".format(round['sequence'],game_user['character'])
+                        await call.bot.edit_message_text(text=text,
+                                                    message_id=game_user['message_id'],
+                                                    chat_id=game_user['id'],
+                                                    reply_markup=draw(cells))
+                else:
+                    await repo.set_round_end(round['id'])
+                    r_w = await repo.get_round_winners(-1, game['id'])
+                    if len(r_w) != 1:
+                        round_id = await repo.create_game_round(game['id'], game_users[0]['id'], sequence=round['sequence'] + 1)
+                        await repo.create_cells(9 , round_id)
+                        charapters = {}
+                        charapters_variants = ["X", "O"]
+                        for i in range(0, len(game_users)):
+                            charapters[game_users[i]['id']] = charapters_variants[i]
+                            await repo.add_user_to_round(game_users[i]['id'], round_id, charapters_variants[i])
+                        cells = await repo.get_game_cells(round_id)
+                        await repo.add_user_steps(9, round_id, user_ids=[i['id'] for i in game_users])
+                        cells = [Cell(cell['id'], "🪑" if not cell['user_id'] else charapters[cell['user_id']], cell['user_id'], cell['round_id'])  for cell in cells]
+                        for i in game_users:
+                            message = await call.message.bot.edit_message_text(chat_id=i['id'],message_id=i['message_id'], text=f"Раунд {round['sequence'] + 1}\n.Вы играете за {charapters[i['id']]}", reply_markup=draw(cells))
+                            await repo.set_game_round_user_message_id(i['id'], round_id, message.message_id)
+                    else:
+                        await repo.set_game_end(game['id'])
+                        for game_user in game_users:
+                            if game_user['id'] == winner_id:
+                                await call.bot.send_message(text=f"Вы победили.\nВам было начислено на счет {rates['value']} фишек", chat_id=game_user['id'])
+                            else:
+                                await call.bot.send_message(text=f"Вы проиграли.\nУ вас было снято со счета {rates['value']} фишек", chat_id=game_user['id'])
         else:
             await call.answer("Поле уже занято", show_alert=True)
     else:
